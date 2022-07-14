f54db2728dbfbd6222d2598931d3ddbc41b6a8fd
mitali.shah <mitali.shah@globant.com>
2022-07-14 12:12:58 +0530

    add bundle product

---
+++ force-app/main/default/lwc/bundleProduct/bundleProduct.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+
+export default class BundleProduct extends LightningElement {
+    handleNext(){
+        const screenEvent = CustomEvent('nextscreen',{
+            detail:'On next Screen'
+            
+         })
+         
+         this.dispatchEvent(screenEvent)
+      }
+      handleBack(){
+        const screenEvent = CustomEvent('backscreen',{
+            detail:'On back Screen'
+            
+         })
+        
+         this.dispatchEvent(screenEvent)
+      }
+}
